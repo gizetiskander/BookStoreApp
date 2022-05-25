66ce50080b3eb04085393f9b34d665c89b663820
Гизетдинов Искандер <gizetiskander@gmail.com>
2022-05-25 12:39:27 +0300

    Models

---
+++ BookStoreApp/BookStoreApp/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ BookStoreApp/BookStoreApp/Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookStoreApp.Models
+{
+    public class Users
+    {
+        public string ID { get; set; }
+        public string FIO { get; set; }
+        public int Phone { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
